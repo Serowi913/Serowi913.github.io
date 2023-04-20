295394c6328d13406eab6728f23454f161f9052d
Serowi913 <73836434+Serowi913@users.noreply.github.com>
2023-04-20 11:20:15 -0400

    Experimented with js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function gameLinkClicked() {
+
+    document.getElementById('header').innerHTML = "DOLPHIN.COM";
+    document.getElementById('caption').innerHTML = "The place for all of your dolphin needs.";
+    document.getElementById('picture_caption').innerHTML = "This is a Dolphin";
+
+}
